3af1c3da8f4ad1095028ee122e4e7c99c7639d5e
code-bebop <57097064+code-bebop@users.noreply.github.com>
2020-04-22 17:45:24 +0900

    2020-04-22 17:44
    HTML 코드를 생성하는 부분을 함수로 바꾸어서 반복을 제거하고 재사용성을 높이는 부분을 배웠다.

--- nodejs/web1_html_internet-master/main.js
+++ nodejs/web1_html_internet-master/main.js
@@ -2,40 +2,47 @@ var http = require('http');
 var url = require('url');
 var fs = require('fs');
 
+function templateList(files) {
+    var list = '<ul>';
+    var i = 0;
+    while (i < files.length) {
+    list += `<li><a href="/?id=${files[i]}">${files[i]}</a></li>`
+      i++;
+    }
+    list += '</ul>';
+  
+    return list;
+}
+
+function templateHTML(title, list, body) {
+  return `
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    ${body}
+  </body>
+  </html>
+  `;
+}
+
 var app = http.createServer(function (request, response) {
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
   var pathName = url.parse(_url, true).pathname;
 
-
   if (pathName === "/") {
     if (queryData.id === undefined) {
-
       fs.readdir("../data", (err, files) => {
         var title = "Welcome";
         var description = "Hello, Node.js";
-        var list = '<ul>';
-        var i = 0;
-        while (i < files.length) {
-          list += `<li><a href="/?id=${files[i]}">${files[i]}</a></li>`
-          i++;
-        }
-        list += '</ul>';
-        var template = `
-        <!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          ${list}
-          <h2>${title}</h2>
-          <p>${description}</p>
-        </body>
-        </html>
-        `;
+        var list = templateList(files);
+        var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
         response.writeHead(200);
         response.end(template);
       })
@@ -43,28 +50,8 @@ var app = http.createServer(function (request, response) {
       fs.readFile(`../data/${queryData.id}`, 'utf8', function (err, description) {
         fs.readdir("../data", (err, files) => {
           var title = queryData.id;
-          var list = '<ul>';
-          var i = 0;
-          while (i < files.length) {
-            list += `<li><a href="/?id=${files[i]}">${files[i]}</a></li>`
-            i++;
-          }
-          list += '</ul>';
-          var template = `
-        <!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          ${list}
-          <h2>${title}</h2>
-          <p>${description}</p>
-        </body>
-        </html>
-        `;
+          var list = templateList(files);
+          var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
           response.writeHead(200);
           response.end(template);
         });
