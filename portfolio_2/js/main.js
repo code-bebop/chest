f5e4ee732a14a76c347838d75fea18fa2f7d2582
code-bebop <57097064+code-bebop@users.noreply.github.com>
2020-05-08 17:46:17 +0900

    2020-05-08 17:39
    slick.js의 옵션 중 fade와 variableWidth를 동시에 적용시키면 정상적으로 작동하지 않는다는 이슈를 발견했다. 이 이슈때문에 오작동을 일으키고 있었던 것이다.
    dots 옵션은 사용하지 않고 "현재 슬라이드 / 총 슬라이드" 로 대체했다.
    그리드 레이아웃 안에서 img가 그리드 아이템의 넓이와 높이를 유지할 수 있도록 수정했다.
    
    이제 슬라이드 바 형식의 nav를 페이지 하단에 넣는 일만 남았다.

--- portfolio_2/js/main.js
+++ portfolio_2/js/main.js
@@ -1,13 +1,23 @@
+function pagingInfo() {
+  $('.slide-ind').text(1 + '/' + $('.slick-slide').length);
+  $('.grid-slide').on('init reInit afterChange', function(event, slick, currentSlide, nextSlide){
+    let i = (currentSlide ? currentSlide : 0) + 1;
+    $('.slide-ind').text(i + '/' + slick.slideCount);
+  });
+}
+
 function slick() {
   $('.grid-slide').slick({
-    arrows: true,
-    dots: true,
-    variableWidth: true
+    dots: false,
+    fade: true,
+    prevArrow: $('.slide-btn-left'),
+    nextArrow: $('.slide-btn-right'),
   });
 }
 
 function init() {
   slick();
+  pagingInfo();
 }
 
 init();
