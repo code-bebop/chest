6ff8cfa16a5dc1c5379943bc013184a13bc7c225
code-bebop <57097064+code-bebop@users.noreply.github.com>
2020-05-11 17:30:51 +0900

    2020-05-11 17:29
    홈페이지 하단에 fold형 네비게이션을 추가했고, 각 아이템을 누르면 본문의 내용이 바뀌고 홈페이지의 전체 색상도 바뀌는 기능도 추가했다.
    이제 디자인만 하면 된다.

--- portfolio_2/js/main.js
+++ portfolio_2/js/main.js
@@ -1,3 +1,56 @@
+function navCon() {
+  const btn = $('.footer__btn'),
+        nav = $(".footer"),
+        navItem = $(".footer__nav__list-item > a"),
+        contents = {
+          "HTML" : [
+            {
+              id : 0,
+              desc : "HTML is ..."
+            }
+          ],
+          "CSS" : [
+            {
+              id : 1,
+              desc : "CSS is ..."
+            }
+          ],
+          "JS" : [
+            {
+              id : 2,
+              desc : "JavaScript is ..."
+            }
+          ]
+        };
+  
+  btn.click((e)=>{
+    nav.toggleClass("on");
+    btnIcon = btn.children();
+    btnIcon.toggleClass("fa-chevron-down").toggleClass("fa-chevron-up");
+  })
+  
+  for(let i=0; i<navItem.length; i++) {
+    navItem[i].addEventListener("click", (e)=>{
+      e.preventDefault();
+      const data = e.target.innerText,
+        background = $("body")[0],
+        content = $(".section_info__body")[0];
+      
+      console.dir(background);
+      if(data==="HTML"){
+        content.innerText = contents.HTML[0].desc;
+        background.style.backgroundColor = "#F5AE92";
+      } else if (data==="CSS") {
+        content.innerText = contents.CSS[0].desc;
+        background.style.backgroundColor = "#F6CCC2";
+      } else if (data==="JS") {
+        content.innerText = contents.JS[0].desc;
+        background.style.backgroundColor = "#7FC4F5";
+      }
+    })
+  }
+}
+
 function pagingInfo() {
   $('.slide-ind').text(1 + '/' + $('.slick-slide').length);
   $('.grid-slide').on('init reInit afterChange', function(event, slick, currentSlide, nextSlide){
@@ -18,7 +71,9 @@ function slick() {
 function init() {
   slick();
   pagingInfo();
+  navCon();
 }
 
-init();
+$(document).ready(init());
+
 
