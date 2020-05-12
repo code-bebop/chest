fb96dd1f157186b97e2dbff338a258c6a21a3f8d
code-bebop <57097064+code-bebop@users.noreply.github.com>
2020-05-12 17:48:34 +0900

    2020-05-12 17:48
    각 nav 아이템에 대한 색상을 정했다.

--- portfolio_2/js/main.js
+++ portfolio_2/js/main.js
@@ -1,8 +1,6 @@
-function navCon() {
-  const btn = $('.footer__btn'),
-        nav = $(".footer"),
-        navItem = $(".footer__nav__list-item > a"),
-        contents = {
+function navEvent() {
+  const navItem = $(".footer__nav__list-item > a"),
+    contents = {
           "HTML" : [
             {
               id : 0,
@@ -23,32 +21,48 @@ function navCon() {
           ]
         };
   
-  btn.click((e)=>{
-    nav.toggleClass("on");
-    btnIcon = btn.children();
-    btnIcon.toggleClass("fa-chevron-down").toggleClass("fa-chevron-up");
-  })
-  
   for(let i=0; i<navItem.length; i++) {
     navItem[i].addEventListener("click", (e)=>{
       e.preventDefault();
       const data = e.target.innerText,
         background = $("body")[0],
+        infoBtn = $(".btn-list__item"),
         content = $(".section_info__body")[0];
       
-      console.dir(background);
       if(data==="HTML"){
         content.innerText = contents.HTML[0].desc;
-        background.style.backgroundColor = "#F5AE92";
+        background.style.backgroundColor = "#FFE9E8";
+        infoBtn.each((index, item)=>{
+          item.style.backgroundColor = "#7EDF9C";
+        })
       } else if (data==="CSS") {
         content.innerText = contents.CSS[0].desc;
-        background.style.backgroundColor = "#F6CCC2";
+        background.style.backgroundColor = "#FFF9D9";
+        infoBtn.each((index, item)=>{
+          item.style.backgroundColor = "#7671E3";
+        })
       } else if (data==="JS") {
         content.innerText = contents.JS[0].desc;
-        background.style.backgroundColor = "#7FC4F5";
+        background.style.backgroundColor = "#DFF4FF";
+        infoBtn.each((index, item)=>{
+          item.style.backgroundColor = "#D6BA96";
+        })
       }
     })
   }
+}
+
+function navFold() {
+  const btn = $('.footer__btn'),
+        nav = $(".footer"),
+        navItem = $(".footer__nav__list-item > a");
+
+  
+  btn.click((e)=>{
+    nav.toggleClass("on");
+    btnIcon = btn.children();
+    btnIcon.toggleClass("fa-chevron-down").toggleClass("fa-chevron-up");
+  })
 }
 
 function pagingInfo() {
@@ -71,7 +85,8 @@ function slick() {
 function init() {
   slick();
   pagingInfo();
-  navCon();
+  navFold();
+  navEvent();
 }
 
 $(document).ready(init());
